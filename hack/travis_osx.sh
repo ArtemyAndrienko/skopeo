d11d173db114d27cbd7e5df0b631cf02ede66c95
Miloslav Trmač <mitr@redhat.com>
2017-08-03 23:07:08 +0200

    Install golint on macOS
    On Linux this is done in the Dockerfile, but macOS tests do not use containers.

--- hack/travis_osx.sh
+++ hack/travis_osx.sh
@@ -8,7 +8,7 @@ _projectatomic="${GOPATH}/src/github.com/projectatomic"
 mkdir -vp ${_projectatomic}
 ln -vsf $(pwd) ${_projectatomic}/skopeo
 
-go get -u github.com/cpuguy83/go-md2man
+go get -u github.com/cpuguy83/go-md2man github.com/golang/lint/golint
 
 cd ${_projectatomic}/skopeo
 make validate-local test-unit-local binary-local
